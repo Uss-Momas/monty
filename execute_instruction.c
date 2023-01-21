2b653206a70ff4aa307acab1acc75b412c129dfc
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-21 14:09:10 +0200

    Function responsable to execute each instruction

---
+++ execute_instruction.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+/**
+ * execute - it executes the right instruction
+ * @stack: the pointer to the stack
+ * @line_number: the number of the line
+ * Return: nothing
+ */
+void execute_instruction(stack_t **stack, unsigned int line_number)
+{
+}
