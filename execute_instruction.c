ea5b11c0a0dc6872fc661098ea09c6f5434f4deb
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-21 18:13:05 +0200

    nop Function that do absolutely nothing

--- execute_instruction.c
+++ execute_instruction.c
@@ -14,6 +14,7 @@ void execute_instruction(stack_t **stack, unsigned int line_number)
 		, {"pop", pop}
 		, {"swap", swap}
 		, {"add", add}
+		, {"nop", nop}
 		, {NULL, NULL}
 	};
 
---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop - doesn't do anything
+ * @stack: head of the stack
+ * @ln: line number
+ * Return: nothing
+ */
+void nop(stack_t **stack, unsigned int ln)
+{
+	(void) *stack;
+	(void) ln;
+}
